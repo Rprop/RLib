aca7005ba03ba9c6babf50463826c2e592811604
in-summer <aiurasaikou@gmail.com>
2016-02-23 22:31:16 +0800

    整理优化

--- rlib-php/ini.php
+++ rlib-php/ini.php
@@ -3,22 +3,24 @@
 
 //### file ###
 
-define('INI_PATH_FUNCTION', $_SERVER['DOCUMENT_ROOT'] . '/functions.php');//函数库路径, 自行修改
-define('INI_PATH_SQL_ERROR', $_SERVER['DOCUMENT_ROOT'] . '/error_sql');//查询错误记录文件路径和文件名, 自行修改
+define('INI_PATH_FUNCTION', $_SERVER['DOCUMENT_ROOT'] . '/rlib-functions.php');//函数库路径和名称(这里为根目录
+define('INI_PATH_SQL_ERROR', $_SERVER['DOCUMENT_ROOT'] . '/error_sql');//查询错误记录文件路径和名称(这里为根目录
 
 
 //### Mysql ###
 
-define('INI_DB_ADDRESS', '数据库的地址');//数据库地址
-define('INI_DB_USERNAME', '数据库的用户名');//mysql用户名
-define('INI_DB_PASSWORD', '数据库密码');//mysql密码
-define('INI_DEFAULT_DBNAME', '默认选择的数据库名');//默认数据库名
-define('INI_CHARSET', 'utf8');//默认编码, 自行修改
+define('INI_DB_ADDRESS', '************');//数据库地址
+define('INI_DB_USERNAME', '************');//mysql用户名
+define('INI_DB_PASSWORD', '************');//mysql密码
+define('INI_DEFAULT_DBNAME', '************');//默认数据库名
+define('INI_CHARSET', 'utf8');//默认编码(这里为utf8
 
 
 //### Others ###
 
-define('INI_ASTARTIME', '1420905600');//agame开始的时间戳
+define('INI_ASTARTIME', '1420905600');//game2开始时间戳
 
+date_default_timezone_set('Asia/Shanghai');//设时区(这里为+8时区
+header("Content-type: text/html; charset=utf-8");//页面编码(这里为utf-8
 
 ?>
--- rlib-php/rlib-functions.php
+++ rlib-php/rlib-functions.php
@@ -1,8 +1,7 @@
 <?php
-/*
+/**
  * 本页面载入的前置是 ini.php
  */
-
 
 
 
@@ -10,7 +9,7 @@
 /**
  * 数据库连接
  * 
- * 参数(可选): 
+ * 参数(可选)(数组): 
  * $host 数据库地址
  * $username 用户名
  * $passwd 密码
@@ -18,7 +17,7 @@
  * 
  * 返回值: 打开的连接(mysql资源)
  * 
- * 2016-02-22
+ * 2016-02-23
  */ 
 function the_mysql_connect($arr = array())
 {
@@ -42,24 +41,32 @@ function the_mysql_connect($arr = array())
  * 
  * 参数1: 数据库连接
  * 参数2: 查询语句
- * 参数3(可选): 调试开关debug 记录开关note
+ * 参数3(可选)(数组): ↓
+ * 是否查询后手动关闭连接close
+ * 调试开关debug 
+ * 记录开关note(既查询成功并记录查询语句)
+ * 错误后继续脚本continue
+ * 默认都为FALSE
  * 
- * 一定要记得先转义需要转义的查询值再传
+ * 要先转义关键值再传值
  * 
  * 返回值: 生结果集(结果集对象)
  * 
- * 2016-02-22
+ * 2016-02-23
  */
 function the_mysql_query($con, $str, $arr = array())
 {
     $debug = FALSE;
     $note = FALSE;
+    $close = FALSE;
+    $continue = FALSE;
     extract($arr);//拆开数组(覆盖)
 
     //$str = mysqli_real_escape_string($con, $str);//转义
     if($debug){echo "[$str]";}//调试用, 直接输出查询的语句
-    $res = mysqli_query($con, $str) or the_mysql_error(array('message' => "数据库查询语句执行出错| 查询语句: {$str}| 错误信息: " . mysqli_error($con)));
-    mysqli_close($con);
+    $res = mysqli_query($con, $str) or the_mysql_error(array('message' => "数据库查询语句执行出错| 查询语句: {$str}| 错误信息: " . mysqli_error($con), 'continue' => $continue));
+    
+    if($close){mysqli_close($con);}
     
     if($note){the_mysql_error(array('message' => $str, 'note' => TRUE));}//记录用
     
@@ -71,17 +78,16 @@ function the_mysql_query($con, $str, $arr = array())
  * 查询语句执行错误记录
  * (日志默认回车符为Unix的)
  * 
- * 参数(可选): 错误信息message(字符串) 记录note(布尔值)(默认为假, 真则不中断脚本)
+ * 参数(可选)(数组): 错误信息message(字符串) 记录note(布尔值)(默认为假, 真则不中断脚本)
  * 
- * 2016-02-22
+ * 2016-02-23
  */
 function the_mysql_error($arr = array())
 {
     $message = '木有错误信息';
     $note = FALSE;
-    extract($arr);//拆开数组(覆盖)
-    
-    date_default_timezone_set('Asia/Shanghai');//设时区
+    $continue = FALSE;
+    extract($arr);
     
     $str = "[ERROR]\n";
     if($note){$str = "[NOTE]\n";}
@@ -92,10 +98,9 @@ function the_mysql_error($arr = array())
     fwrite($handle, $str);
     fclose($handle);
     
-    if(!$note){die('啊.代码又出bug了..db');}
+    if($note == FALSE && $continue == FALSE){die('数据库错误');}
     
     return TRUE;
-    
 }
 
 
@@ -119,63 +124,27 @@ function tosql($con, $str)
 
 
 /**
- * 只查询一行的简易查询
- * 
- * 参数1: 查询的字段
- * 参数2: 表名
- * 参数3: 查询条件
- * 参数4(可选): 数据库连接相关的用户名密码库名等的更改
- * 
- * 返回值: 数组(一行结果mysqli_fetch_array处理后的数组)
-
-function the_mysql_sel_one($ziduan, $table, $tiaojian, $arr = array())
-{
-    $str = "SELECT {$ziduan} FROM {$table} WHERE {$tiaojian} LIMIT 1";
-    $res = the_mysql_go($str ,$arr);
-    $res = mysqli_fetch_array($res);
-    return $res;
-}
- */
-
-
-/**
- * 数字转曜日
+ * 数字转星期
  * 
  * 参数1: 0-6 (0->星期日 6->星期六) 如:date('w')
+ * 参数2(可选)(数组): 返回值种类type 默认为0
+ * 可自行修改函
+ * 返回值: 星期几(字符串)
  * 
- * 返回值: 某曜日(字符串)
- * 
- * 2016-02-22
+ * 2016-02-23
  */
-function yaori($week_num)
+function the_week($week_num, $arr = array())
 {
-    switch($week_num)
-    {
-        case 0: 
-            return '日曜日';
-            break;
-        
-        case 1: 
-            return '月曜日';
-            break;
-        case 2: 
-            return '火曜日';
-            break;
-        case 3: 
-            return '水曜日';
-            break;
-        case 4: 
-            return '木曜日';
-            break;
-        case 5: 
-            return '金曜日';
-            break;
-        case 6: 
-            return '土曜日';
-            break;
-        default:
-            return '异时空曜日';
-    }
+    $type = 0;
+    $week_word_str[0] = '星期日|星期一|星期二|星期三|星期四|星期五|星期六';//以"|"分割
+    $week_word_str[1] = '日曜日|月曜日|火曜日|水曜日|木曜日|金曜日|土曜日';
+    extract($arr);
+    
+    $week_word_arr = explode('|', $week_word_str[$type]);
+
+    $week = isset($week_word_arr[$week_num]) ? $week_word_arr[$week_num] : '异时空星期';
+    
+    return $week;
 }
 
 
@@ -186,36 +155,57 @@ function yaori($week_num)
  * 
  * 返回值: 季节信息(字符串)
  * 
- * 2016-02-22
+ * 2016-02-23
  */
-function season($month)
+function the_season($month)
 {
-    switch($month)
-    {
-        case 12:
-        case 1:
-        case 2: 
-            return '冬';
-            break;
-        case 3:
-        case 4:
-        case 5: 
-            return '春';
-            break;
-        case 6:
-        case 7:
-        case 8: 
-            return '夏';
-            break;
-        case 9:
-        case 10:
-        case 11: 
-            return '秋';
-            break;
-        default:
-            return '异时空季节';
-    }
+    $month--;
+    $season_num = floor($month/3);
+    $season_arr = explode('|', '冬|春|夏|秋');
+    
+    $season = isset($season_arr[$season_num]) ? $season_arr[$season_num] : '异时空季节';
+    return $season;
 }
+
+
+
+/**
+ * 通过ip查询所属地
+ * 用到了91查的API
+ * 
+ * 参数1: ip地址
+ * 
+ * 返回值: 地区信息(字符串)
+ * 
+ * 2016-02-23
+ */
+function the_local($ip)
+{
+    $the_url = 'http://api.91cha.com/ip?key=7043ca58366c4388a3333499d46ed325&ip='.$ip;
+    $con = curl_init();//句柄
+    curl_setopt($con, CURLOPT_URL, $the_url);//地址
+    curl_setopt($con, CURLOPT_TIMEOUT, 5);//超时
+    curl_setopt($con, CURLOPT_RETURNTRANSFER, 1);//不直接输出
+    $re = curl_exec($con);//一库
+    curl_close($con);//关闭
+    
+    if($re)
+    {
+        $info = json_decode($re, TRUE);
+        if($info['state'] != 1)
+        {
+            $info = FALSE;
+        }
+    }
+    else
+    {
+        $info = FALSE;
+    }
+    
+    
+    return $info;
+}
+
 
 
 /**
@@ -224,11 +214,11 @@ function season($month)
  * 
  * 参数1: ip地址
  * 
- * 返回值: 天气信息(多维数组)
+ * 返回值: 天气信息(多维数组)/获取失败则为FALSE
  * 
- * 2016-02-22
+ * 2016-02-23
  */
-function weather($ip)//(返回数组)
+function the_weather($ip)//(返回数组)
 {
     $the_url = 'http://route.showapi.com/9-4?showapi_appid=15850&showapi_sign=05a656c81fb54aa2b01e6bba6ea232b6&showapi_timestamp=' . date('YmdHis') . '&ip=' . $ip . '&needAlarm=1';
     
@@ -241,23 +231,27 @@ function weather($ip)//(返回数组)
     $re = curl_exec($con);//一库
     curl_close($con);//关闭
     
-    $info = json_decode($re, TRUE);
+    if($re)
+    {
+        $info = json_decode($re, TRUE);
+        if($info['showapi_res_code'] === 0)
+        {
+            isset($info['showapi_res_body']['f1']['jiangshui']) or $info['showapi_res_body']['f1']['jiangshui'] = '暂无';//暂时只知道这个有时会没有
+        }
+        else
+        {
+            $info = FALSE;
+        }
+    }
+    else
+    {
+        $info = FALSE;
+    }
     
-    //*有时不造为何抽风就没了些数据//**觉得这么写好笨...
-    isset($info['showapi_res_body']['cityInfo']['c7']) or $info['showapi_res_body']['cityInfo']['c7'] = '暂无';
-    isset($info['showapi_res_body']['cityInfo']['c5']) or $info['showapi_res_body']['cityInfo']['c5'] = '暂无';
-    isset($info['showapi_res_body']['now']['temperature']) or $info['showapi_res_body']['now']['temperature'] = '暂无';
-    isset($info['showapi_res_body']['f1']['day_air_temperature']) or $info['showapi_res_body']['f1']['day_air_temperature'] = '暂无';
-    isset($info['showapi_res_body']['f1']['night_air_temperature']) or $info['showapi_res_body']['f1']['night_air_temperature'] = '暂无';
-    isset($info['showapi_res_body']['now']['sd']) or $info['showapi_res_body']['now']['sd'] = '暂无';
-    isset($info['showapi_res_body']['now']['wind_power']) or $info['showapi_res_body']['now']['wind_power'] = '暂无';
-    isset($info['showapi_res_body']['f1']['jiangshui']) or $info['showapi_res_body']['f1']['jiangshui'] = '暂无';
-    isset($info['showapi_res_body']['now']['weather']) or $info['showapi_res_body']['now']['weather'] = '暂无';
-    isset($info['showapi_res_body']['f1']['day_weather']) or $info['showapi_res_body']['f1']['day_weather'] = '暂无';
-    isset($info['showapi_res_body']['f1']['night_weather']) or $info['showapi_res_body']['f1']['night_weather'] = '暂无';
+    
     return $info;
-    //return var_dump($the_url);
 }
+
 
 
 /**
@@ -432,7 +426,7 @@ $agent = $_SERVER['HTTP_USER_AGENT'];
     {
       $os = '未知操作系统';
     }
-    return $os;  
+    return $os;
 }
 
 
@@ -443,7 +437,6 @@ $agent = $_SERVER['HTTP_USER_AGENT'];
  * 
  * 返回值: 语言信息(字符串) / $_SERVER["HTTP_ACCEPT_LANGUAGE"]
  * 
- * 2016-02-22
  */
 function getLanguage()
 {
@@ -462,26 +455,6 @@ function getLanguage()
 
 
 
-/**
- * 通过ip查询所属地
- * 用到了91查的API
- * 
- * 参数1: ip地址
- * 
- * 返回值: 地区信息(字符串)
- * 
- * 2016-02-22
- */
-function getLocal($ip)
-{
-    $url = 'http://api.91cha.com/ip?key=7043ca58366c4388a3333499d46ed325&ip='.$ip;
-    $all = file_get_contents($url);
-    $ccc = json_decode($all,TRUE);
-    //$all = iconv("GB2312","utf8",$allz);
-    $out = $ccc['data']['country'].'|'.$ccc['data']['area'].'|'.$ccc['data']['province'].'|'.$ccc['data']['city'].'|'.$ccc['data']['district'].'|'.$ccc['data']['linetype'];
-    return $out;
-}
-
 
 /**
  * 获取访问者ip(貌似不受网页代理影响)
@@ -490,9 +463,8 @@ function getLocal($ip)
  * 
  * 返回值: ip地址(字符串)
  * 
- * 2016-02-22
  */
-function get_user_ip()
+function getIP()
 {
     if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown'))
     {
@@ -513,65 +485,5 @@ function get_user_ip()
     return preg_match("/[\d\.]{7,15}/", $ip, $matches) ? $matches[0] : 'unknown';
 }
 
-
-/**
- * 转义html元素并直接输出 (如 < 成为 &lt;)
- * 
- * 参数: 要转义的字符串
- * 
- * 返回值: 无
- * 
- * 2016-02-22
- */
-function echohtml($input_daima)
-{
-    echo htmlspecialchars($input_daima,ENT_QUOTES,"UTF-8");
-}
-
-
-
-
-/**
- * 转义html元素 (如 < 成为 &lt;)
- * 
- * 参数: 要转义的字符串
- * 
- * 返回值: 转义后的结果(字符串)
- * 
- * 2016-02-22
- */
-function varhtml($input_daima) //
-{
-    return htmlspecialchars($input_daima,ENT_QUOTES,"UTF-8");
-}
-
-
-
-
-/**
- * 特殊符号前面加斜杠转义
- * 
- * 参数: 要转义的字符串
- * 
- * 返回值: 转义后的结果(字符串)
- * 
- * 2016-02-22
- */
-function tophp($value)
-{
-
-    if (!get_magic_quotes_gpc())
-    {
-        $value = addslashes($value);
-    }
-/* 如果不是数字则加引号
-    
-    if (!is_numeric($value))
-    {
-        $value = "'" . mysql_real_escape_string($value) . "'";
-    }
-*/  
-    return $value;
-}
 
 ?>
