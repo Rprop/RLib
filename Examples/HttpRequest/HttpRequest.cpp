a3a4dd61dc2fefd81ff1c916ac37a9f322e928b5
rrrfff <rrrfff@foxmail.com>
2016-07-18 19:35:54 +0800

    add HttpRequest example

---
+++ Examples/HttpRequest/HttpRequest.cpp
@@ -0,0 +1,27 @@
+/********************************************************************
+	Created:	2016/07/18  19:15
+	Filename: 	HttpRequest.cpp
+	Author:		rrrfff
+	Url:	    http://blog.csdn.net/rrrfff
+*********************************************************************/
+#include <RLib_Import.h>
+
+//-------------------------------------------------------------------------
+
+int __stdcall WinMain(__in HINSTANCE /*hInstance*/, __in_opt HINSTANCE /*hPrevInstance*/,
+					  __in LPSTR /*lpCmdLine*/, __in int /*nShowCmd*/)
+{
+	// WebClient is a wrapper class for HttpRequest/HttpResponse,
+	// and platform-independent in a sense.
+
+	// performs a HTTP GET request and gets response as string
+	string blog = WebClient::GetResponseText(_R("http://rlib.cf/"));
+
+	// also, HTTPS/SSL is supported.
+	string ssl_page = WebClient::GetResponseText(_R("https://www.alipay.com/"));
+
+	// performs a HTTP POST request with string data
+	string page = WebClient::PostResponseText(_R("http://rlib.cf/"), _R("post data"));
+
+	return STATUS_SUCCESS;
+}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 RLib
 ====
 
-RLib(also named CppFrame, rFrame) is a lightweight, highly cohesive and low coupling framework of useful toolkit that aims to make common cpp development more effectively and easily. 
+RLib is a lightweight, highly cohesive and low coupling framework of useful toolkit that aims to make cpp development more effectively and easily. 
 
 Currently, RLib has covered a range of modules including IO, Net, XML, Text, Memory, Thread, Collection, and other high performance, reuseable tasks.             
 
